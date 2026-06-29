c07d495f3a9706db58a20ea37c9558fa6cb048be
Dinesh-Gokul <dinesh.gokul@avvaitech.com>
2026-06-29 17:28:33 +0530

    29-06-2026 Afternoon Changes

--- backend/routes/dataRoutes.js
+++ backend/routes/dataRoutes.js
@@ -117,6 +117,12 @@ router.post("/numberseriessearchdata",dataController.getnumberseriessearchdata)
 router.post("/addNumberseries",dataController.addNumberseries)
 router.post("/NumberSeriesUpdate",dataController.NumberSeriesUpdate)
 router.post("/NumberSeriesdeleteData",dataController.numberseriesdeleteData)
+router.post("/GYM_TrainerInsert",dataController.GYM_TrainerInsert)
+router.post("/GYM_TrainerUpdate",dataController.GYM_TrainerUpdate)
+router.post("/GYM_TrainerDelete",dataController.GYM_TrainerDelete)
+router.post("/GYM_TrainerLoopInsert",dataController.GYM_TrainerLoopInsert)
+router.post("/GYM_TrainerLoopUpdate",dataController.GYM_TrainerLoopUpdate)
+router.post("/GYM_TrainerLoopDelete",dataController.GYM_TrainerLoopDelete)
 
 
 
